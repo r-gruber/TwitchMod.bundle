56fc26b54692e8ee7575c3600c42170d61667fca
sander1 <sander@plexapp.com>
2013-03-22 04:52:52 +0100

    Updates, some layout changes, fixes.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -6,97 +6,122 @@
 
 ####################################################################################################
 
-TWITCH_LIST_STREAMS     = 'https://api.twitch.tv/kraken/streams'
+TWITCH_LIST_STREAMS = 'https://api.twitch.tv/kraken/streams'
 TWITCH_FEATURED_STREAMS = 'https://api.twitch.tv/kraken/streams/featured'
-TWITCH_TOP_GAMES        = 'https://api.twitch.tv/kraken/games/top'
-TWITCH_SEARCH_STREAMS   = 'https://api.twitch.tv/kraken/search/streams'
-TWITCH_LIVE_PLAYER      = 'http://www-cdn.jtvnw.net/widgets/live_embed_player.swf?auto_play=true'
+TWITCH_TOP_GAMES = 'https://api.twitch.tv/kraken/games/top'
+TWITCH_SEARCH_STREAMS = 'https://api.twitch.tv/kraken/search/streams'
+TWITCH_LIVE_PLAYER = 'http://www-cdn.jtvnw.net/widgets/live_embed_player.swf?auto_play=true'
 
-PAGE_LIMIT              = 100
-CACHE_INTERVAL          = 600
-NAME                    = 'Twitch.tv'
-ART                     = 'art-default.jpg'
-ICON                    = 'icon-default.png'
+PAGE_LIMIT = 100
+NAME = 'Twitch.tv'
+ART = 'art-default.jpg'
+ICON = 'icon-default.png'
 
 ####################################################################################################
 def Start():
 
-    ObjectContainer.art = R(ART)
-    ObjectContainer.title1 = NAME
-    DirectoryItem.thumb = R(ICON)
-    
-    HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:19.0) Gecko/20100101 Firefox/19.0'
+	ObjectContainer.art = R(ART)
+	ObjectContainer.title1 = NAME
+	DirectoryItem.thumb = R(ICON)
+
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:19.0) Gecko/20100101 Firefox/19.0'
+	HTTP.CacheTime = 600
 
 ####################################################################################################
 @handler('/video/twitch', NAME, thumb=ICON, art=ART)
 def MainMenu():
-    oc = ObjectContainer()
-	oc.add(DirectoryObject(key=Callback(FeaturedStreamsMenu), title="Featured Streams"))
-    oc.add(DirectoryObject(key=Callback(GamesMenu), title="Games", summary="Browse live streams by game"))
-    oc.add(InputDirectoryObject(key=Callback(SearchResults), title="Search", prompt="Search For A Stream", summary="Search for a stream"))
 
-    return oc
+	oc = ObjectContainer()
+	oc.add(DirectoryObject(key=Callback(FeaturedStreamsMenu), title="Featured Streams"))
+	oc.add(DirectoryObject(key=Callback(GamesMenu), title="Games", summary="Browse live streams by game"))
+	oc.add(InputDirectoryObject(key=Callback(SearchResults), title="Search", prompt="Search For A Stream", summary="Search for a stream"))
+
+	return oc
 
 ####################################################################################################
 @route('/video/twitch/featured')
 def FeaturedStreamsMenu():
 
-    oc = ObjectContainer(title2="Featured Streams")
-    url  = "%s?limit=%s" % (TWITCH_FEATURED_STREAMS, PAGE_LIMIT)
-    featured = JSON.ObjectFromURL(url, cacheTime=CACHE_INTERVAL)
+	oc = ObjectContainer(title2="Featured Streams")
+	url = "%s?limit=%s" % (TWITCH_FEATURED_STREAMS, PAGE_LIMIT)
+	featured = JSON.ObjectFromURL(url)
 
-    for stream in featured['featured']:
-        subtitle = "%s\n%s Viewers" % (stream['stream']['game'], stream['stream']['viewers'])
-        summary = String.StripTags(stream['text'])
-        streamUrl = "%s&channel=%s" % (TWITCH_LIVE_PLAYER, stream['stream']['channel']['name'])
-        oc.add(VideoClipObject(url=streamUrl, title=stream['stream']['channel']['display_name'], thumb=stream['stream']['preview'], subtitle=subtitle))
+	for stream in featured['featured']:
+		subtitle = "%s\n%s Viewers" % (stream['stream']['game'], stream['stream']['viewers'])
+		summary = String.StripTags(stream['text'])
+		stream_url = "%s&channel=%s" % (TWITCH_LIVE_PLAYER, stream['stream']['channel']['name'])
 
-        return oc
+		oc.add(VideoClipObject(
+			url = stream_url,
+			title = stream['stream']['channel']['display_name'],
+			summary = '%s\n\n%s' % (subtitle, summary),
+			thumb = stream['stream']['preview']
+		))
+
+	return oc
 
 ####################################################################################################
 @route('/video/twitch/games', page=int)
 def GamesMenu(page=0):
 
-    oc = ObjectContainer(title2="Top Games")
-    url  = "%s?limit=%s&offset=%s" % (TWITCH_TOP_GAMES, PAGE_LIMIT, page*PAGE_LIMIT)
-    games = JSON.ObjectFromURL(url, cacheTime=CACHE_INTERVAL)
+	oc = ObjectContainer(title2="Top Games")
+	url = "%s?limit=%s&offset=%s" % (TWITCH_TOP_GAMES, PAGE_LIMIT, page*PAGE_LIMIT)
+	games = JSON.ObjectFromURL(url)
 
-    for game in games['top']:
-        gameSummary = "%s Channels\n%s Viewers" % (game['channels'], game['viewers'])
-        oc.add(DirectoryObject(key=Callback(ChannelMenu, game=game['game']['name'])), title=game['game']['name'], summary=gameSummary, thumb=game['game']['logo']['large'])
+	for game in games['top']:
+		game_summary = "%s Channels\n%s Viewers" % (game['channels'], game['viewers'])
 
-    if(len(games['top']) == 100):
-        oc.add(DirectoryObject(key=Callback(GamesMenu, title = "More Games", page = (page+1))))
+		oc.add(DirectoryObject(
+			key = Callback(ChannelMenu, game=game['game']['name']),
+			title = game['game']['name'],
+			summary = game_summary,
+			thumb = game['game']['logo']['large']
+		))
 
-    return oc
+	if len(games['top']) == 100:
+		oc.add(DirectoryObject(key=Callback(GamesMenu, title="More Games", page=page+1)))
+
+	return oc
 
 ####################################################################################################
 @route('/video/twitch/channel')
-def ChannelMenu(game=None):
+def ChannelMenu(game):
 
-    oc = ObjectContainer(title2=game)
-    url = "%s?game=%s&limit=%s" % (TWITCH_LIST_STREAMS, String.Quote(game, usePlus=True), PAGE_LIMIT)
-    streams = JSON.ObjectFromURL(url, cacheTime=CACHE_INTERVAL)
+	oc = ObjectContainer(title2=game)
+	url = "%s?game=%s&limit=%s" % (TWITCH_LIST_STREAMS, String.Quote(game, usePlus=True), PAGE_LIMIT)
+	streams = JSON.ObjectFromURL(url)
 
-    for stream in streams['streams']:
-        subtitle = " %s Viewers" % stream['viewers']
-        streamURL = "%s&channel=%s" % (TWITCH_LIVE_PLAYER, stream['channel']['name'])
-        oc.add(VideoClipObject(url=streamUrl, title=stream['channel']['display_name'], summary=stream['channel']['status'],thumb=stream['channel']['logo'], subtitle=subtitle, duration=0)))        
+	for stream in streams['streams']:
+		subtitle = " %s Viewers" % stream['viewers']
+		stream_url = "%s&channel=%s" % (TWITCH_LIVE_PLAYER, stream['channel']['name'])
 
-    return oc
+		oc.add(VideoClipObject(
+			url = stream_url,
+			title = stream['channel']['display_name'],
+			summary = '%s\n\n%s' % (subtitle, stream['channel']['status']),
+			thumb = stream['channel']['logo']
+		))
+
+	return oc
 
 ####################################################################################################
 def SearchResults(query=''):
 
-    oc = ObjectContainer()
-    results = JSON.ObjectFromURL("%s?query=%s&limit=%s" % (TWITCH_SEARCH_STREAMS, String.Quote(query, usePlus=True), PAGE_LIMIT), cacheTime=CACHE_INTERVAL)
+	oc = ObjectContainer()
+	results = JSON.ObjectFromURL("%s?query=%s&limit=%s" % (TWITCH_SEARCH_STREAMS, String.Quote(query, usePlus=True), PAGE_LIMIT))
 
-    for stream in results['streams']:
-        subtitle = "%s\n%s Viewers" % (stream['game'], stream['viewers'])
-        streamURL = "%s&channel=%s" % (TWITCH_LIVE_PLAYER, stream['channel']['name'])
-        oc.add(VideoClipObject(url=streamUrl, title=stream['channel']['display_name'], summary=stream['channel']['status'],thumb=stream['channel']['logo'], subtitle=subtitle, duration=0)))        
+	for stream in results['streams']:
+		stream_url = "%s&channel=%s" % (TWITCH_LIVE_PLAYER, stream['channel']['name'])
+		subtitle = "%s\n%s Viewers" % (stream['game'], stream['viewers'])
 
-    if len(oc) < 1:
-        return ObjectContainer(header="Not found", message="No streams were found that match your query.")
+		oc.add(VideoClipObject(
+			url = stream_url,
+			title = stream['channel']['display_name'],
+			summary = '%s\n\n%s' % (subtitle, stream['channel']['status']),
+			thumb = stream['channel']['logo']
+		))
 
-    return oc
+	if len(oc) < 1:
+		return ObjectContainer(header="Not found", message="No streams were found that match your query.")
+
+	return oc
