80c79ac85721f1b38b5c22a6456a87ef6dab5a89
sander1 <sander@plexapp.com>
2013-05-16 14:01:03 +0200

    Use hosted resources

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -13,27 +13,22 @@ TWITCH_SEARCH_STREAMS = 'https://api.twitch.tv/kraken/search/streams'
 
 PAGE_LIMIT = 100
 NAME = 'Twitch.tv'
-ART = 'art-default.jpg'
-ICON = 'icon-default.jpg'
 
 ####################################################################################################
 def Start():
 
-	ObjectContainer.art = R(ART)
 	ObjectContainer.title1 = NAME
-	DirectoryObject.thumb = R(ICON)
-
-	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:19.0) Gecko/20100101 Firefox/19.0'
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0'
 	HTTP.CacheTime = 600
 
 ####################################################################################################
-@handler('/video/twitch', NAME, thumb=ICON, art=ART)
+@handler('/video/twitch', NAME)
 def MainMenu():
 
 	oc = ObjectContainer()
 	oc.add(DirectoryObject(key=Callback(FeaturedStreamsMenu), title="Featured Streams"))
 	oc.add(DirectoryObject(key=Callback(GamesMenu), title="Games", summary="Browse Live Streams by Game"))
-	oc.add(InputDirectoryObject(key=Callback(SearchResults), title="Search", prompt="Search for a Stream", summary="Search for a Stream", thumb=R('search.png')))
+	oc.add(InputDirectoryObject(key=Callback(SearchResults), title="Search", prompt="Search for a Stream", summary="Search for a Stream"))
 
 	return oc
 
@@ -57,7 +52,7 @@ def FeaturedStreamsMenu():
 			url = stream['stream']['channel']['url'],
 			title = stream['stream']['channel']['display_name'],
 			summary = '%s\n\n%s' % (subtitle, summary),
-			thumb = Resource.ContentsOfURLWithFallback(stream['stream']['preview']['large'], fallback='icon-default.jpg')
+			thumb = Resource.ContentsOfURLWithFallback(stream['stream']['preview']['large'])
 		))
 
 	return oc
@@ -77,7 +72,7 @@ def GamesMenu(page=0):
 			key = Callback(ChannelMenu, game=game['game']['name']),
 			title = game['game']['name'],
 			summary = game_summary,
-			thumb = Resource.ContentsOfURLWithFallback(game['game']['logo']['large'], fallback='icon-default.jpg')
+			thumb = Resource.ContentsOfURLWithFallback(game['game']['logo']['large'])
 		))
 
 	if len(games['top']) == 100:
@@ -103,7 +98,7 @@ def ChannelMenu(game):
 			url = stream['channel']['url'],
 			title = stream['channel']['display_name'],
 			summary = '%s\n\n%s' % (subtitle, stream['channel']['status']),
-			thumb = Resource.ContentsOfURLWithFallback(stream['channel']['logo'], fallback='icon-default.jpg')
+			thumb = Resource.ContentsOfURLWithFallback(stream['channel']['logo'])
 		))
 
 	return oc
@@ -124,7 +119,7 @@ def SearchResults(query=''):
 			url = stream['channel']['url'],
 			title = stream['channel']['display_name'],
 			summary = '%s\n\n%s' % (subtitle, stream['channel']['status']),
-			thumb = Resource.ContentsOfURLWithFallback(stream['channel']['logo'], fallback='icon-default.jpg')
+			thumb = Resource.ContentsOfURLWithFallback(stream['channel']['logo'])
 		))
 
 	if len(oc) < 1:
